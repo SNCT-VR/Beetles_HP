c7329cb230d4bd1bdaae93f5005f70c05715c396
sansai-SNCT <syunzo1115@icloud.com>
2022-09-27 16:16:41 +0900

    [update] 未使用のインポートを削除

--- src/components/common/ModelDraw/index.tsx
+++ src/components/common/ModelDraw/index.tsx
@@ -1,7 +1,7 @@
 import React, { FC, Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { useLoader } from '@react-three/fiber'
-import { Environment, OrbitControls } from '@react-three/drei'
+import { OrbitControls } from '@react-three/drei'
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
 
 const Scene = () => {
