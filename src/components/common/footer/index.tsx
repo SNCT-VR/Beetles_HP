ec390d65dc5b8ba7e56d08bf2ffa7a7feda5d432
sansai-SNCT <syunzo1115@icloud.com>
2022-09-28 02:15:08 +0900

    [create] footerの実装

---
+++ src/components/common/footer/index.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react'
+import tw from 'twin.macro'
+import { Text } from '../Text'
+
+const FooterContainar = tw.footer` text-center`
+
+const Footer: FC = () => (
+  <FooterContainar>
+    <Text>&copy; 成績底辺の会</Text>
+  </FooterContainar>
+)
+
+export { Footer }
