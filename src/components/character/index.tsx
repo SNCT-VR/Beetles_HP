2fc300f81169c7e1bdb90466a93f52e0fb3d6e87
sansai-SNCT <syunzo1115@icloud.com>
2022-09-27 16:15:28 +0900

    [add] キャラクターコンポーネントの実装

---
+++ src/components/character/index.tsx
@@ -0,0 +1,19 @@
+import React, { FC } from 'react'
+import tw from 'twin.macro'
+import ModelDraw from '../common/ModelDraw'
+import { Text } from '../common/Text'
+import { Title } from '../common/Title'
+
+interface CharacterInterface {
+  title: string
+}
+const CharacterContainar = tw.div`space-y-2`
+const Character: FC<CharacterInterface> = ({ title }) => {
+  return (
+    <CharacterContainar>
+      <Title>{title}</Title>
+      <ModelDraw />
+    </CharacterContainar>
+  )
+}
+export { Character }
