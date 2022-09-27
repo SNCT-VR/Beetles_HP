c5d9bc2ee21e1bd701619f3abf74b5c22eca42a1
sansai-SNCT <syunzo1115@icloud.com>
2022-09-27 15:32:34 +0900

    [add] fbxファイルを描写するコンポーネントを実装描写するコンポーネントを実装

---
+++ src/components/common/ModelDraw/index.tsx
@@ -0,0 +1,25 @@
+import React, { FC, Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { useLoader } from '@react-three/fiber'
+import { Environment, OrbitControls } from '@react-three/drei'
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
+
+const Scene = () => {
+  const fbx = useLoader(FBXLoader, '/assets/bee/bee.fbx')
+
+  return <primitive object={fbx} scale={0.05} />
+}
+
+const ModelDraw: FC = () => {
+  return (
+    <Canvas>
+      <Suspense fallback={null}>
+        <ambientLight />
+        <Scene />
+        <OrbitControls />
+      </Suspense>
+    </Canvas>
+  )
+}
+
+export default ModelDraw
