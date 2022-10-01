1d99e86f7ec69fe5f74086b25b2bc943551c5c14
sansai-SNCT <syunzo1115@icloud.com>
2022-10-02 00:20:40 +0900

    [update] imgのパスを個別に変更

--- src/data/profile.ts
+++ src/data/profile.ts
@@ -8,25 +8,25 @@ export const profiles: Props[] = [
   {
     name: 'Akane Matuda',
     role: 'リーダー , モデル制作 , パーツ加工',
-    imageUrl: '/data/new_icon.jpeg',
+    imageUrl: '/assets/image/akane.jpeg',
     alt: "akane's Icon",
   },
   {
     name: 'Keisuke Tominaga',
     role: 'ハードウェアの設計&制御',
-    imageUrl: '/data/new_icon.jpeg',
+    imageUrl: '/assets/image/tommy.jpeg',
     alt: "keisuke's Icon",
   },
   {
     name: 'Keigo Kondo',
     role: 'ゲーム制作全般 , 回路制作など',
-    imageUrl: '/data/new_icon.jpeg',
+    imageUrl: '/assets/image/kondo.jpeg',
     alt: "keigo's Icon",
   },
   {
     name: 'Shunta Yamagiwa',
     role: 'ゲーム制作アシスト , HP制作',
-    imageUrl: '/data/new_icon.jpeg',
+    imageUrl: '/assets/image/sansai.jpg',
     alt: "shunta's Icon",
   },
 ]
