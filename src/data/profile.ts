a8b436ea63ef5a66c37fac523e105bba8ea1b7a3
sansai-SNCT <syunzo1115@icloud.com>
2022-10-02 22:58:24 +0900

    [update] roleの修正

--- src/data/profile.ts
+++ src/data/profile.ts
@@ -7,19 +7,19 @@ interface Props {
 export const profiles: Props[] = [
   {
     name: 'Akane Matuda',
-    role: 'リーダー , モデル制作 , パーツ加工',
+    role: 'リーダー , モデル制作 , ハードウェア設計',
     imageUrl: '/assets/image/akane.jpeg',
     alt: "akane's Icon",
   },
   {
     name: 'Keisuke Tominaga',
-    role: 'ハードウェアの設計&制御',
+    role: 'ハードウェア制御 , 回路設計',
     imageUrl: '/assets/image/tommy.jpeg',
     alt: "keisuke's Icon",
   },
   {
     name: 'Keigo Kondo',
-    role: 'ゲーム制作全般 , 回路制作など',
+    role: 'ゲーム制作全般 , 回路制作',
     imageUrl: '/assets/image/kondo.jpeg',
     alt: "keigo's Icon",
   },
