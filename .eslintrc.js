564d02f0f65bde5d7172447e252ef0d12280f1f7
sansai-SNCT <syunzo1115@icloud.com>
2022-10-05 23:49:19 +0900

    [fix] fix: eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,18 +4,73 @@ module.exports = {
     es2021: true,
   },
   extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
     'eslint:recommended',
-    'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'prettier',
   ],
-  overrides: [],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
+    project: './tsconfig.json',
   },
-  plugins: ['react', '@typescript-eslint'],
+  plugins: ['@typescript-eslint'],
   rules: {
-    'react/no-unknown-property': ['error', { ignore: ['object'] }],
+    'no-console': [
+      'warn',
+      {
+        allow: ['warn', 'info', 'error'],
+      },
+    ],
+    // console.logが残っていればwarn
+    'prefer-arrow-callback': 'error',
+    // arrow functionを許可
+    'prefer-const': 'error',
+    // const推奨
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/explicit-module-boundary-types': 'error',
+    '@typescript-eslint/consistent-type-imports': [
+      'warn',
+      {
+        prefer: 'type-imports',
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        varsIgnorePattern: '^_',
+        argsIgnorePattern: '^_',
+      },
+    ],
+    // 未使用変数はエラー
+    'import/prefer-default-export': 'off',
   },
+  overrides: [
+    // 一部ルールを除外する
+    {
+      files: ['src/pages/**/*.tsx'],
+      // pagesのdefault exportは仕方ないので除外
+      rules: {
+        'import/no-default-export': 'off',
+      },
+    },
+    {
+      files: ['**/*.tsx'],
+      // componentの戻り値の型定義の記述は必須にしない
+      rules: {
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        'react/no-unknown-property': ['error', { ignore: ['object'] }],
+        'react/function-component-definition': [
+          2,
+          {
+            namedComponents: 'arrow-function',
+          },
+        ],
+      },
+    },
+  ],
 }
