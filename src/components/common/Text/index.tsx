acb4a9b18a123c46b83d8bac4641f8a4d00453b1
sansai-SNCT <syunzo1115@icloud.com>
2022-10-09 11:26:38 +0900

    [add] add fonts

--- src/components/common/Text/index.tsx
+++ src/components/common/Text/index.tsx
@@ -1,5 +1,5 @@
 import tw from 'twin.macro'
 
-const Text = tw.p`text-base select-none text-masara`
+const Text = tw.p`text-base select-none text-masara font-zen font-medium`
 
 export { Text }
--- src/components/common/Title/index.tsx
+++ src/components/common/Title/index.tsx
@@ -1,5 +1,5 @@
 import tw from 'twin.macro'
 
-const Title = tw.h1`text-4xl select-none text-masara font-bold`
+const Title = tw.h1`text-4xl select-none text-masara font-zen font-bold`
 
 export { Title }
