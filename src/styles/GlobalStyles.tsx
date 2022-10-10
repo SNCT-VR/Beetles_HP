3af7cc15004dbcecd4e1beddcebb1ead95cc9173
sansai-SNCT <syunzo1115@icloud.com>
2022-10-10 11:05:12 +0900

    [add] Import ZenKakuGothic

--- src/styles/GlobalStyles.tsx
+++ src/styles/GlobalStyles.tsx
@@ -1,13 +1,20 @@
 import React from 'react'
-import { Global } from '@emotion/react'
-import tw, { css, theme, GlobalStyles as BaseStyles } from 'twin.macro'
+import { css, Global } from '@emotion/react'
+import { GlobalStyles as BaseStyles } from 'twin.macro'
 
-const customStyles = css({
-  body: {
-    WebkitTapHighlightColor: theme`colors.purple.500`,
-    ...tw`antialiased`,
-  },
-})
+const customStyles = css`
+  @font-face {
+    font-family: 'Zen KakuGothic New';
+    src: url('/fonts/Zen/ZenKakuGothicNew.woff2') format('woff2'),
+      url('/fonts/Zen/ZenKakuGothicNew.woff') format('woff'),
+      url('/fonts/Zen/ZenKakuGothicNew.ttf') format('truetype'),
+      url('/fonts/Zen/ZenKakuGothicNew.eot') format('embedded-opentype');
+    font-style: normal;
+    font-weight: 400;
+    /* https://styled-components.com/docs/faqs#how-do-i-fix-flickering-text-after-server-side-rendering */
+    font-display: fallback;
+  }
+`
 
 const GlobalStyles = () => (
   <>
