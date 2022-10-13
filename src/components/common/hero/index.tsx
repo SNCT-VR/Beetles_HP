07122c0936a3a9747005b2b15155bae150aaf304
sansai-SNCT <syunzo1115@icloud.com>
2022-10-13 12:05:30 +0900

    [update] Change scroll button display

--- src/components/common/hero/index.tsx
+++ src/components/common/hero/index.tsx
@@ -8,9 +8,9 @@ interface HeroInterface {
   children: React.ReactNode
 }
 
-const HeroContainer = tw.div`h-screen text-center flex flex-col`
+const HeroContainer = tw.div`h-[calc((100vh)/2)] md:h-screen text-center flex flex-col`
 const Image = tw.img` m-auto scale-75 md:scale-100 md:h-2/5`
-const ScrollButton = tw.button` bg-masara text-white font-bold animate-bounce w-48 h-24  rounded-full m-auto`
+const ScrollButton = tw.button` bg-masara text-white font-bold animate-bounce w-48 h-24  rounded-full m-auto hidden md:block`
 
 const scroller = () =>
   window.scrollTo({
