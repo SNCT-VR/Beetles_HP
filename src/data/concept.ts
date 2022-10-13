d60b46ec07278423ca436bcb53dc48426c6129cc
sansai-SNCT <syunzo1115@icloud.com>
2022-10-13 15:24:36 +0900

    [add] Add consepts data

---
+++ src/data/concept.ts
@@ -0,0 +1,23 @@
+interface Props {
+  alt: string
+  imgUrl: string
+}
+
+export const consepts: Props[] = [
+  {
+    alt: 'ゲームのコンセプト画像1',
+    imgUrl: '/image/consepts/exp1.png',
+  },
+  {
+    alt: 'ゲームのコンセプト画像2',
+    imgUrl: '/image/consepts/exp4.png',
+  },
+  {
+    alt: 'ハチの説明画像',
+    imgUrl: '/image/consepts/exp3.png',
+  },
+  {
+    alt: '人間の説明画像',
+    imgUrl: '/image/consepts/exp6.png',
+  },
+]
