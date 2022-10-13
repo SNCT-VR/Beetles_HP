445bdc72090a73ad7556b6bc93b618c02c33899c
sansai-SNCT <syunzo1115@icloud.com>
2022-10-13 20:00:55 +0900

    [fix] Improved accessibility

--- src/components/member/index.tsx
+++ src/components/member/index.tsx
@@ -12,7 +12,7 @@ interface MemberInterface {
 }
 
 const MemberContainer = tw.div`space-y-2 text-center`
-const ProfileBox = tw.div`space-y-1.5  w-full md:w-1/2 my-4`
+const ProfileBox = tw.li`space-y-1.5  w-full md:w-1/2 my-4`
 const FlexBox = tw.ul`flex justify-center flex-wrap`
 
 const Member: FC<MemberInterface> = ({ title }) => (
